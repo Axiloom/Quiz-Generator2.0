879c7e687b89843cc2581013ce8c543247bf4634
John Bednarczyk <johnbednarczykjr@gmail.com>
2019-04-22 22:16:54 -0500

    Adjust button size

--- src/SaveMenu.java
+++ src/SaveMenu.java
@@ -1,13 +1,10 @@
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.geometry.Insets;
-import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
-import javafx.scene.image.Image;
-import javafx.scene.image.ImageView;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
