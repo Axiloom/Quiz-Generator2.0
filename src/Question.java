7ca9c5f494c2be0efafbdad3c3fdde582bf1fa34
msutr_000 <msutr_000@10.141.108.182>
2019-04-24 17:35:20 -0500

    Added json reader to resources

--- src/Question.java
+++ src/Question.java
@@ -1,6 +1,15 @@
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.ArrayList;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import java.io.FileNotFoundException; 
+import java.io.PrintWriter;
+import org.json.simple.JSONArray; 
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException; 
+
 
 public class Question {
 
@@ -76,9 +85,14 @@ public class Question {
    * 
    * @param jsonFilePath
    * @return
+ * @throws ParseException 
+ * @throws IOException 
+ * @throws FileNotFoundException 
    */
-  public boolean loadJSON(String jsonFilePath) {
+  public boolean loadJSON(String jsonFilePath) throws FileNotFoundException, IOException, ParseException {
     // Make as many private helper methods as we need
+	Object obj = new JSONParser().parse(new FileReader(jsonFilePath));
+	
     return true;
   }
   
