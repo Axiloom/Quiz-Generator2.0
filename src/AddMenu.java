008fd05ae62899556e58dca4712a1a4465ff5693
John Bednarczyk <johnbednarczykjr@gmail.com>
2019-04-27 19:04:27 -0500

    Possible fixed addMenu alignment

--- src/AddMenu.java
+++ src/AddMenu.java
@@ -107,25 +107,28 @@ public class AddMenu extends Main {
     BorderPane topicPane = new BorderPane();
     topicPane.setLeft(topicLabel);
     topicPane.setRight(topic);
-    topicPane.setMaxWidth(320);
+    topicPane.setMaxWidth(350);
 
     Label questionLabel = new Label("Enter Question: ");
     BorderPane questionPane = new BorderPane();
     questionPane.setLeft(questionLabel);
     questionPane.setRight(question);
-    questionPane.setMaxWidth(320);
+    questionPane.setMaxWidth(350);
     questionPane.setPadding(new Insets(0,0,10,0));
 
     Label answerLabel = new Label("Enter Answer: ");
     BorderPane answerPane = new BorderPane();
     answerPane.setLeft(answerLabel);
     answerPane.setRight(answer);
+    answerPane.setMaxWidth(350);
     
     Label optionsLabel = new Label("Enter Alternative Options: ");
     BorderPane optionPane = new BorderPane();
     optionPane.setLeft(optionsLabel);
     VBox optionsBox = new VBox(option1, option2, option3, option4);
     optionPane.setRight(optionsBox);
+    optionPane.setMaxWidth(350);
+
 
 
     Label or = new Label("OR");
