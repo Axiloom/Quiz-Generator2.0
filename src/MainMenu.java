a0d146771ba3c19b76fe78cb85b2fbac71492f16
jalessner <jalessner@wisc.edu>
2019-04-29 19:13:11 -0500

    MainMenu final clean-up

--- src/MainMenu.java
+++ src/MainMenu.java
@@ -1,3 +1,27 @@
+///////////////////////////////////////////////////////////////////////////////
+//
+// Assignment: Quiz-Generator Team Project
+// Due: 5-2-19
+// Title: Main Menu 
+// Files: MainMenu.java
+// Course: CS 400, Spring 2019, Lec 001
+//
+// Authors: A-Team 99 
+//          (John Bednarczyk, Joseph Lessner, Joshua Liberko, Shefali Mukerji, Mitchell Sutrick)
+// Lecturer's Name: Deb Deppeler
+//
+///////////////////////////// CREDIT OUTSIDE HELP /////////////////////////////
+//
+// Students who get help from sources other than their partner must fully
+// acknowledge and credit those sources of help here. Instructors and TAs do
+// not need to be credited here, but tutors, friends, relatives, room mates,
+// strangers, and others do. If you received no outside help from either type
+// of source, then please explicitly indicate NONE.
+//
+// Persons: NONE
+// Online Sources: NONE
+//
+///////////////////////////////////////////////////////////////////////////////
 
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
@@ -33,10 +57,11 @@ public class MainMenu extends Main {
   private Button save; // save button
   private Button start; // start quiz button
   private Alert alert; // alert displayed when improperly starting quiz
-  private ListView<String> selected;
-  private MenuButton choices;
-  private List<CheckMenuItem> topics;
-  private TextField numberOfQuestions;
+  private ListView<String> selected; // ListView of topics selected
+  private MenuButton choices; // Drop down menu of selectable topics
+  private List<CheckMenuItem> topics; // full list of available topics to be selected
+  private TextField numberOfQuestions; // TextField for entering number of questions
+
   /**
    * MainMenu Constructor that declares the field variables
    * 
@@ -53,8 +78,9 @@ public class MainMenu extends Main {
   }
 
   /**
-   * Initalize MainMenu
-   * @return root
+   * Initalizes a BorderPane of the MainMenu scene
+   * 
+   * @return root - BorderPane of the MainMenu scene
    */
   public BorderPane initialize() {
 
@@ -63,10 +89,10 @@ public class MainMenu extends Main {
     setLeftPanel();
     setRightPanel();
     setBottomPanel();
-    
+
     return root;
   }
-  
+
   /**
    * Sets the background to an image
    */
@@ -76,10 +102,10 @@ public class MainMenu extends Main {
     Image image1 = new Image("uwCrest_Dope.png");
     Background background = new Background(new BackgroundImage(image1, BackgroundRepeat.NO_REPEAT,
         BackgroundRepeat.NO_REPEAT, BackgroundPosition.CENTER, bSize));
-    
+
     root.setBackground(background);
   }
-  
+
   /**
    * Constructs the top panel in the BorderPane
    */
@@ -98,10 +124,10 @@ public class MainMenu extends Main {
     topPanel.setSpacing(100);
     topPanel.setAlignment(Pos.CENTER);
     topPanel.setStyle("-fx-background-color: #9fb983");
-    
+
     root.setTop(topPanel);
   }
-  
+
   /**
    * Constructs the left panel in the BorderPane
    */
@@ -109,42 +135,42 @@ public class MainMenu extends Main {
     // Style
     add.setPrefWidth(180);
     save.setPrefWidth(180);
-    
+
     // Listeners
     save.setOnAction(event -> primaryStage.setScene(Main.getSaveScene()));
     add.setOnAction(event -> primaryStage.setScene(Main.getAddScene()));
-    
+
     // Left Panel
     VBox leftVBox = new VBox(add, save);
     leftVBox.setAlignment(Pos.TOP_RIGHT);
     leftVBox.setPadding(new Insets(20, 0, 0, 20));
-    
+
     root.setLeft(leftVBox);
   }
-  
+
   /**
    * Constructs the right panel in the BorderPane
    */
   private void setRightPanel() {
 
     // Topics ComboBox
-    choices = new MenuButton ( "Set Topics" ) ;
-    choices.setPrefWidth(180);
+    choices = new MenuButton("Set Topics");
+    choices.setPrefWidth(200);
 
     // List of topics
     topics.clear(); // clear list before adding more topics to avoid repeats
 
-    for (String s : Main.getQuestion().getTopics()){
+    for (String s : Main.getQuestion().getTopics()) {
       topics.add(new CheckMenuItem(s));
     }
-    
+
     // Clear selected so that chosen topic questions aren't still there when you go back and
     // load more questions
     selected.getItems().clear();
-    
+
     choices.getItems().addAll(topics);
 
-    for  (CheckMenuItem item : topics)  {
+    for (CheckMenuItem item : topics) {
       item.selectedProperty().addListener((observableValue, oldValue, newValue) -> {
         if (newValue)
           selected.getItems().add(item.getText());
@@ -164,7 +190,7 @@ public class MainMenu extends Main {
 
     root.setRight(rightVBox);
   }
-  
+
   /**
    * Constructs the bottom panel in the BorderPane
    */
@@ -178,7 +204,7 @@ public class MainMenu extends Main {
     start.setOnAction(event -> {
 
       // Error Message
-      if(selected.getItems().isEmpty() || numberOfQuestions.getText().isEmpty()) {
+      if (selected.getItems().isEmpty() || numberOfQuestions.getText().isEmpty()) {
         alert = new Alert(Alert.AlertType.ERROR, "Select a topic and number of questions");
         alert.setHeaderText("Error.");
         alert.showAndWait().filter(response -> response == ButtonType.OK);
@@ -194,7 +220,7 @@ public class MainMenu extends Main {
         Random rand = new Random();
 
         // Pick Random Questions
-        for (int i = 0 ; i < Integer.parseInt(numberOfQuestions.getText()) ; i++){
+        for (int i = 0; i < Integer.parseInt(numberOfQuestions.getText()); i++) {
           String topic = selected.getItems().get(rand.nextInt(selected.getItems().size()));
           int questionNum = rand.nextInt(Main.getQuestion().getQuestions(topic).size());
           questions.add(Main.getQuestion().getQuestions(topic).get(questionNum));
@@ -204,7 +230,7 @@ public class MainMenu extends Main {
         Main.setupStatisticsScene(Integer.parseInt(numberOfQuestions.getText()));
 
         // Reset all topics and #'s
-        for (CheckMenuItem item : topics){
+        for (CheckMenuItem item : topics) {
           item.setSelected(false);
         }
 
@@ -213,12 +239,12 @@ public class MainMenu extends Main {
         primaryStage.setScene(Main.getQuizScene());
       }
     });
-    
+
     // Bottom Panel
     HBox bottomHBox = new HBox(start);
     bottomHBox.setPadding(new Insets(0, 0, 65, 0));
     bottomHBox.setAlignment(Pos.CENTER);
-    
+
     root.setBottom(bottomHBox);
   }
 }
