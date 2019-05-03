35fec39b0f609f739e153f2aeb1511a852f31943
John Bednarczyk <johnbednarczykjr@gmail.com>
2019-05-02 23:32:33 -0500

    Created new zip

---
+++ resources/team/application/Question.java
@@ -0,0 +1,350 @@
+package team.application;
+
+///////////////////////////////////////////////////////////////////////////////
+//
+// Assignment: Quiz-Generator Team Project
+// Due: 5-2-19
+// Title: application.Question
+// Files: application.Question.java
+// Course: CS 400, Spring 2019, Lec 001
+//
+// Authors: A-Team 99 
+//          (John Bednarczyk, Joseph Lessner, Joshua Liberko, Shefali Mukerji, Mitchell Sutrick)
+// Lecturer's Name: Deb Deppeler
+//
+///////////////////////////// CREDIT OUTSIDE HELP /////////////////////////////
+//
+// Students who get help from sources other than their partner must fully
+// acknowledge and credit those sources of help here. Instructors and TAs do
+// not need to be credited here, but tutors, friends, relatives, room mates,
+// strangers, and others do. If you received no outside help from either type
+// of source, then please explicitly indicate NONE.
+//
+// Persons: NONE
+// Online Sources: NONE
+//
+///////////////////////////////////////////////////////////////////////////////
+
+import javafx.scene.control.Alert;
+import javafx.scene.control.ButtonType;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.Reader;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Hashtable;
+import java.util.List;
+import java.util.Optional;
+import java.util.Random;
+
+/**
+ * application.Main application.Question Class to retrieve anything question related
+ * 
+ * @author ATeam-99
+ */
+public class Question {
+
+  private Hashtable<String, ArrayList<QuestionNode>> topics; // Hash table to store questions
+  private int numQuestions; // total number of questions available
+  private Alert alert; // alert displayed when saving
+
+  /**
+   * Each node represents a single question and all attributes associated with that question
+   * 
+   * @author ATeam-99
+   *
+   */
+  class QuestionNode {
+    String metadata; // metadata for a question
+    String question; // question text
+    ArrayList<String> options; // all options as answers
+    String answer; // correct answer
+    ImageView img; // image going along with the question
+    String imageName; // retains the name of the image
+    String topic; // topic associated with this question
+
+    /**
+     * Each node represents a single question and all attributes associated with that question
+     *
+     * @param topic - the topic associated with this question
+     * @param metadata - the metadata associated with this question
+     * @param questionText - the text associated with this question
+     * @param image - the image associated with this question
+     * @param options - the possible answers associated with this question
+     * @param correctAnswer - the correct answer for this questions
+     */
+    QuestionNode(String topic, String metadata, String questionText, String image,
+        ArrayList<String> options, String correctAnswer) {
+
+
+      if (!image.equals("none")) {
+        Image tempImg = new Image(image);
+        img = new ImageView(tempImg);
+        imageName = image;
+      } else {
+        img = null;
+        imageName = "none";
+      }
+
+      this.metadata = metadata;
+      this.question = questionText;
+      this.options = options;
+      this.answer = correctAnswer;
+      this.topic = topic;
+    }
+  }
+
+  /**
+   * No-arg Constructor for application.Question class
+   */
+  public Question() {
+    topics = new Hashtable<>();
+    numQuestions = 0;
+  }
+
+  /**
+   * Returns an ArrayList of questions for a particular topic group in random order
+   * 
+   * @param topic the topic which we want to search for
+   * @param numQuestions the number of questions from that topic
+   */
+  public ArrayList<QuestionNode> getQuestions(String topic, int numQuestions) {
+
+    // Random generator
+    Random rand = new Random();
+
+    // Array to store our questions
+    ArrayList<QuestionNode> questions = new ArrayList<>();
+
+    // Get random questions
+    for (int i = 0; i < numQuestions; i++) {
+      QuestionNode randomQuestion = topics.get(topic).get(rand.nextInt(topics.get(topic).size()));
+
+      while (questions.contains(randomQuestion)) {
+        randomQuestion = topics.get(topic).get(rand.nextInt(topics.get(topic).size()));
+      }
+      questions.add(randomQuestion);
+    }
+    return questions;
+  }
+
+  /**
+   * Gets an ArrayList of QuestionNodes containing the questions for a given topic
+   * 
+   * @param topic - topic of questions being retrieved
+   * @return ArrayList<QuestionNode> of questions
+   */
+  public ArrayList<QuestionNode> getQuestions(String topic) {
+    return topics.get(topic);
+  }
+
+  /**
+   * Adds a node to the topicList according to its topic
+   *
+   * @param topic - the topic of the question
+   * @param questionText - the text of the question
+   * @param metadata - the meta of the question
+   * @param options - the options for the question
+   * @param correctAnswer - the correct answer for the question
+   * @param image - the image for the question
+   */
+  public void addQuestion(String topic, String questionText, String metadata,
+      ArrayList<String> options, String correctAnswer, String image) {
+
+    // application.Question to be added
+    QuestionNode question = new QuestionNode(topic, metadata, questionText, image, options, correctAnswer);
+
+    // Create topic if it doesnt exist
+    if (!topics.containsKey(question.topic))
+      topics.put(question.topic, new ArrayList<>());
+
+    // Add question
+    topics.get(question.topic).add(question);
+    ++numQuestions;
+  }
+
+  /**
+   * Parses a json file and adds the information to the data fields
+   * 
+   * @param jsonFilePath the path to the JSON file
+   * @throws ParseException error with parsing
+   * @throws IOException error with reading file
+   * @throws FileNotFoundException file does not exist
+   */
+  public void loadJSON(String jsonFilePath)
+      throws FileNotFoundException, IOException, ParseException {
+
+    Reader reader =
+            new InputStreamReader(this.getClass().getResourceAsStream(File.separator + jsonFilePath));
+
+    Object obj = new JSONParser().parse(reader);
+
+    JSONObject jo = (JSONObject)obj;
+
+    JSONArray questionArray = (JSONArray) jo.get("questionArray");
+
+    for (Object aQuestionArray : questionArray) {
+      JSONObject jsonQuestion = (JSONObject) aQuestionArray;
+      String metaData = (String) jsonQuestion.get("meta-data");
+      String question = (String) jsonQuestion.get("questionText");
+      String topic = (String) jsonQuestion.get("topic");
+      String image = (String) jsonQuestion.get("image");
+      // need to interate through the choices
+      JSONArray answerArray = (JSONArray) jsonQuestion.get("choiceArray");
+      ArrayList<String> choices = new ArrayList<>();
+      String correctAnswer = "";
+      for (Object answer : answerArray) {
+        if (((JSONObject) answer).get("isCorrect").equals("T")) {
+          correctAnswer = (String) ((JSONObject) answer).get("choice");
+        }
+        choices.add((String) ((JSONObject) answer).get("choice"));
+      }
+
+      // Add application.Question
+      addQuestion(topic, question, metaData, choices, correctAnswer, image);
+    }
+
+    ((JSONObject) obj).clear();
+  }
+
+  /**
+   * Saves all questions in a new json file, checking for duplicates
+   * 
+   * @param jsonFile - name of the new JSON file
+   * @return true if the file was successfully saved, false otherwise
+   */
+  @SuppressWarnings("unchecked")
+  public boolean saveToJSON(String jsonFile) {
+
+    JSONObject questionJSON = new JSONObject();
+    JSONArray questionArray = new JSONArray();
+    List<String> topicList = getTopics();
+
+    if (topicList.size() == 0) {
+      alert = new Alert(Alert.AlertType.WARNING, "There are no questions to save.");
+      alert.setHeaderText("Error:");
+      alert.showAndWait().filter(response -> response == ButtonType.OK);
+      return false;
+    }
+
+    for (String topic : topicList) {
+
+      ArrayList<application.Question.QuestionNode> questionList = getQuestions(topic);
+
+      for (QuestionNode question : questionList) {
+
+        // Create a new JSONObject
+        JSONObject aQuestion = new JSONObject();
+
+        aQuestion.put("meta-data", question.metadata);
+        aQuestion.put("questionText", question.question);
+        aQuestion.put("topic", question.topic);
+        aQuestion.put("image", question.imageName);
+
+        JSONArray choiceArray = new JSONArray();
+
+        for (String option : question.options) {
+
+          JSONObject opt = new JSONObject();
+
+          if (option.equals(question.answer)) {
+            opt.put("isCorrect", "T");
+            opt.put("choice", option);
+          }
+          else {
+            opt.put("isCorrect", "F");
+            opt.put("choice", option);
+          }
+          choiceArray.add(opt);
+        }
+        aQuestion.put("choiceArray", choiceArray);
+        questionArray.add(aQuestion);
+      }
+    }
+    questionJSON.put("questionArray", questionArray);
+
+    try {
+
+      File f1 = new File(jsonFile + ".json");
+
+      Optional<ButtonType> button;
+
+      if (!f1.createNewFile()) { // checks for duplicate file already existing
+
+        // Offer to override existing file
+        alert = new Alert(Alert.AlertType.CONFIRMATION, "Overwrite " + jsonFile + ".json?");
+        alert.setHeaderText("File Already Exists.");
+        button = alert.showAndWait();
+        if (button.get().equals(ButtonType.OK)) { // Overwrites
+          FileWriter file = new FileWriter(f1);
+          file.write(questionJSON.toJSONString());
+          file.flush();
+          file.close();
+        }
+        if (button.get().equals(ButtonType.CANCEL)) { // cancels save
+          return false;
+        }
+      } else { // Save
+        FileWriter file = new FileWriter(jsonFile + ".json");
+        file.write(questionJSON.toJSONString());
+        file.flush();
+        file.close();
+      }
+    } catch (Exception e) {
+      e.printStackTrace();
+      return false;
+    }
+    return true;
+  }
+
+  /**
+   * Gets the total number of questions available across all topics
+   * 
+   * @return the total number of questions
+   */
+  public int getSize() {
+    return numQuestions;
+  }
+
+  /**
+   * Gets the number of questions available for a particular topic
+   * 
+   * @param topic - the topic which we are finding number of questions for
+   * @return number of questions for a specific topic, or -1 if topic does not exist
+   */
+  public int getSize(String topic) {
+
+    if (topic == null)
+      return -1;
+
+    else if (!topics.containsKey(topic)) {
+      return 0;
+    }
+
+    return topics.get(topic).size();
+  }
+
+  /**
+   * Gets a set of alphabetized topics
+   *
+   * @return List containing all topics in the data set
+   */
+  public List<String> getTopics() {
+
+    List<String> alphabetizedList = new ArrayList<>(topics.keySet());
+
+    Collections.sort(alphabetizedList);
+
+    return alphabetizedList;
+  }
+}
---
+++ resources/team/application/StatisticsMenu.java
@@ -0,0 +1,169 @@
+package team.application;
+
+///////////////////////////////////////////////////////////////////////////////
+//
+// Assignment: Quiz-Generator Team Project
+// Due: 5-2-19
+// Title: Statistics Menu 
+// Files: application.StatisticsMenu.java
+// Course: CS 400, Spring 2019, Lec 001
+//
+// Authors: A-Team 99 
+//          (John Bednarczyk, Joseph Lessner, Joshua Liberko, Shefali Mukerji, Mitchell Sutrick)
+// Lecturer's Name: Deb Deppeler
+//
+///////////////////////////// CREDIT OUTSIDE HELP /////////////////////////////
+//
+// Students who get help from sources other than their partner must fully
+// acknowledge and credit those sources of help here. Instructors and TAs do
+// not need to be credited here, but tutors, friends, relatives, room mates,
+// strangers, and others do. If you received no outside help from either type
+// of source, then please explicitly indicate NONE.
+//
+// Persons: NONE
+// Online Sources: NONE
+//
+///////////////////////////////////////////////////////////////////////////////
+
+import application.Main;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontWeight;
+import javafx.stage.Stage;
+
+/**
+ * application.StatisticsMenu Class constructs the GUI for the application.StatisticsMenu to display the quiz results.
+ * 
+ * @author ATeam-99
+ *
+ */
+public class StatisticsMenu extends application.Main {
+
+  private Stage primaryStage; // stage being displayed on
+  private BorderPane root; // BorderPane being constructed
+  private Button cont; // continue button to start new quiz
+  private Button exit; // exit button
+  protected double correct; // number of correctly answered questions
+  private double numQuestions; // total number of questions in the current quiz
+
+  /**
+   * application.StatisticsMenu Constructor that declares the field variables and sets the background color
+   * 
+   * @param primaryStage - stage being displayed on
+   */
+  public StatisticsMenu(Stage primaryStage) {
+    this.primaryStage = primaryStage;
+    root = new BorderPane();
+    cont = new Button("NEW QUIZ");
+    exit = new Button("EXIT");
+    root.setStyle("-fx-background-color: #c0c0c5");
+    correct = (double) 0;
+  }
+
+  /**
+   * Initializes a BorderPane of the application.StatisticsMenu screen
+   * 
+   * @return root - BorderPane of the application.StatisticsMenu screen
+   */
+  public BorderPane initialize(int numQuestions) {
+
+    this.numQuestions = (double) numQuestions;
+
+    setTopPanel();
+    setCenterPanel();
+    setBottomPanel();
+
+    return root;
+  }
+
+  /**
+   * Constructs the top panel in the BorderPane
+   */
+  private void setTopPanel() {
+    // Label
+    Label label = new Label("Statistics Menu");
+
+    // Style
+    label.setFont(Font.font("Arial", FontWeight.BOLD, 16));
+
+    // Top Panel
+    HBox topPanel = new HBox(label);
+    topPanel.setPadding(new Insets(10, 50, 10, 50));
+    topPanel.setSpacing(100);
+    topPanel.setAlignment(Pos.CENTER);
+    topPanel.setStyle("-fx-background-color: #9fb983");
+
+    root.setTop(topPanel);
+  }
+
+  /**
+   * Constructs the center panel in the BorderPane
+   */
+  private void setCenterPanel() {
+    // Calculate score
+    Double pct = (correct / numQuestions) * 100;
+    String percent = pct.toString();
+    if (percent.length() > 5) {
+      percent = percent.substring(0, 5);
+    }
+    Label percentCorrect = new Label(percent + "% Correct");
+    Label fractionCorrect = new Label(((int) correct) + " / " + (int) numQuestions);
+    Label score = new Label("Your score");
+    Label blank = new Label(""); // spacer
+
+    // Style
+    score.setFont(Font.font("Times New Roman", FontWeight.EXTRA_BOLD, 40));
+    percentCorrect.setFont(Font.font("Arial", FontWeight.BOLD, 30));
+    fractionCorrect.setFont(Font.font("Arial", FontWeight.BOLD, 30));
+
+    // Center Panel
+    VBox centerVBox = new VBox(score, blank, fractionCorrect, percentCorrect);
+    centerVBox.setAlignment(Pos.CENTER);
+    score.setStyle("-fx-underline: true");
+
+    root.setCenter(centerVBox);
+  }
+
+  /**
+   * Constructs the bottom panel in the BorderPane
+   */
+  private void setBottomPanel() {
+    // Style
+    cont.setPrefSize(150, 50);
+    exit.setPrefSize(150, 50);
+    cont.setOnMouseEntered(e -> cont.setStyle("-fx-font-size: 14pt;"));
+    cont.setOnMouseExited(e -> cont.setStyle("-fx-font-size: 12pt;"));
+    exit.setOnMouseEntered(e -> exit.setStyle("-fx-font-size: 14pt;"));
+    exit.setOnMouseExited(e -> exit.setStyle("-fx-font-size: 12pt;"));
+
+    // Listeners
+    cont.setOnAction(e -> {
+      correct = 0; // Reset number of correct answers
+      primaryStage.setScene(application.Main.getMainScene());
+    });
+
+    exit.setOnAction(e -> {
+      primaryStage.setScene(Main.getExitScene());
+    });
+
+    // Bottom Panel
+    HBox bottomHBox = new HBox(cont, exit);
+    bottomHBox.setPadding(new Insets(0, 100, 65, 150));
+    bottomHBox.setSpacing(100);
+
+    root.setBottom(bottomHBox);
+  }
+
+  /**
+   * Update number of correct answers by 1
+   */
+  public void updateCount() {
+    correct++;
+  }
+}
